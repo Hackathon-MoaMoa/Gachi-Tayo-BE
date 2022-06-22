951f93a817eb4b197143449a76238b9b74dd1e1a
kyunji <kyunji@ajou.ac.kr>
2022-06-23 02:09:21 +0900

    ADD: PostDto, PostMapper 생성

--- src/main/java/com/example/gachitayobe/dto/PostDto.java
+++ src/main/java/com/example/gachitayobe/dto/PostDto.java
@@ -1,12 +1,23 @@
 package com.example.gachitayobe.dto;
 
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Getter
 public class PostDto {
     private int uId;
     private int pId;
-    private String password;
-    private int gender;
-    private String phone;
-    private String name;
-    private int score;
-
+    private double startX;
+    private double startY;
+    private String startAddress;
+    private String startName;
+    private double endX;
+    private double endY;
+    private String endAddress;
+    private String endName;
+    private Timestamp startDate;
+    private int totalMember;
+    private int currentMember;
+    private int done;
 }
