ae25d8205b091a8d1168dd151e07a793ea102690
kyunji <kyunji@ajou.ac.kr>
2022-06-23 23:44:37 +0900

    ADD: kakao 로컬 api 연동(위도,경도 변환)

--- src/main/java/com/example/gachitayobe/controller/PostController.java
+++ src/main/java/com/example/gachitayobe/controller/PostController.java
@@ -1,12 +1,18 @@
 package com.example.gachitayobe.controller;
 
-import com.example.gachitayobe.dto.PostSearchDto;
+import com.example.gachitayobe.service.PostSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class PostController {
-    @GetMapping("/api/posts")
-    public void getPosts (@RequestParam PostSearchDto postSearchDto){
+    @Autowired
+    private PostSearchService postSearchService;
 
+    @GetMapping("/test/kakao/coordinate")
+    public ArrayList<Double> getPosts (@RequestParam String query){
+        return postSearchService.getCoordinate(query);
     }
 }
