caa2321689428559823f708cae282d4696193fc2
seonyounggg <psy8961@naver.com>
2022-06-23 21:22:14 +0900

    FEAT : 남은 좌석 조회

--- src/main/java/com/example/gachitayobe/service/PostService.java
+++ src/main/java/com/example/gachitayobe/service/PostService.java
@@ -3,6 +3,9 @@ package com.example.gachitayobe.service;
 import com.example.gachitayobe.dto.PostDetailDto;
 import com.example.gachitayobe.dto.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDetailDto getPostDetail(int p_id);
+    List<Integer> getRemainSeat(int p_id);
 }
--- src/main/java/com/example/gachitayobe/service/PostServiceImpl.java
+++ src/main/java/com/example/gachitayobe/service/PostServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.gachitayobe.mapper.PostMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostServiceImpl implements PostService {
     @Autowired
@@ -16,5 +18,9 @@ public class PostServiceImpl implements PostService {
 
     public PostDetailDto getPostDetail(int p_id){
         return postMapper.getPostDetail(p_id);
+    }
+
+    public List<Integer> getRemainSeat(int p_id){
+        return postMapper.getRemainSeat(p_id);
     }
 }
