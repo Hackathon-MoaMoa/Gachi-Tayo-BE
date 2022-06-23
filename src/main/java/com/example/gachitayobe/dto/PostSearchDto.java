ba492b7fc0d3c5bc0173a25a00dcc767207d62cf
Moonyujin <yujin0419@sookmyung.ac.kr>
2022-06-24 04:44:18 +0900

    FIX: 충돌해결

--- src/main/java/com/example/gachitayobe/dto/PostSearchDto.java
+++ src/main/java/com/example/gachitayobe/dto/PostSearchDto.java
@@ -1,16 +1,9 @@
 package com.example.gachitayobe.dto;
 
 import lombok.Data;
-<<<<<<< HEAD
-import lombok.Getter;
-=======
->>>>>>> 858bf2eceda4f2bf1cf45e586f73e743a2bbd36f
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 858bf2eceda4f2bf1cf45e586f73e743a2bbd36f
 @Data
 public class PostSearchDto {
     private String startAddress;
--- src/main/java/com/example/gachitayobe/dto/PostXYDto.java
+++ src/main/java/com/example/gachitayobe/dto/PostXYDto.java
@@ -9,9 +9,6 @@ public class PostXYDto {
     private double endX;
     private double endY;
     private int radius;
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 858bf2eceda4f2bf1cf45e586f73e743a2bbd36f
 }
